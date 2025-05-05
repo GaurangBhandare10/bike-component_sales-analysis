8091afd02615b56c3aac922552b3194587bed4e9
Gaurang Bhandare <92206122+GaurangBhandare10@users.noreply.github.com>
2025-05-05 21:44:20 +0530

    Update performance_analysis.sql

--- scripts/performance_analysis.sql
+++ scripts/performance_analysis.sql
@@ -1,3 +1,16 @@
+/*
+===============================================================================
+Performance Analysis (Year-over-Year)
+===============================================================================
+Purpose:
+    - To measure the performance of products, customers, or regions over time.
+    - For benchmarking and identifying high-performing entities.
+    - To track yearly trends and growth.
+
+ Analyzed the yearly performance of products by comparing their sales 
+to both the average sales performance of the product and the previous year's sales */
+
+
 WITH yearly_product_sales AS (
     SELECT
         YEAR(f.order_date) AS order_year,
@@ -28,3 +41,20 @@ LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS py_sa
         ELSE 'Avg'
     END AS py_change
 from yearly_product_sales
+
+
+--SAMPLE OUTPUT
+
+-- | Year | Product               | Sales | Avg Sales | Prev Year Sales | Performance   |   Trend   |
+-- |------|------------------------|-------|-----------|------------------|-------------|-----------|
+-- | 2012 | All-Purpose Bike Stand | 159   | 13197     | NULL             | Below Avg   | Avg       |
+-- | 2013 | All-Purpose Bike Stand | 37683 | 13197     | 159              | Above Avg   | Increase  |
+-- | 2014 | All-Purpose Bike Stand | 1749  | 13197     | 37683            | Below Avg   | Decrease  |
+-- | 2012 | AWC Logo Cap           | 72    | 6570      | NULL             | Below Avg   | Avg       |
+-- | 2013 | AWC Logo Cap           | 18891 | 6570      | 72               | Above Avg   | Increase  |
+-- | 2014 | AWC Logo Cap           | 747   | 6570      | 18891            | Below Avg   | Decrease  |
+-- | 2013 | Bike Wash - Dissolver  | 6960  | 3636      | NULL             | Above Avg   | Avg       |
+-- | 2014 | Bike Wash - Dissolver  | 312   | 3636      | 6960             | Below Avg   | Decrease  |
+-- | 2013 | Classic Vest- L        | 11968 | 6240      | NULL             | Above Avg   | Avg       |
+-- | 2014 | Classic Vest- L        | 512   | 6240      | 11968            | Below Avg   | Decrease  |
+
